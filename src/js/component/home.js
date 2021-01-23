7980856ad34d231dfa8e1e7d6baedb1529caf96c
Mauricio Lizcano <mauriciolizcano25531@gmail.com>
2021-01-23 18:22:22 -0400

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,33 +1,28 @@
 import React, { useEffect, useState } from "react";
 
-//component
 function Home() {
 	const BASE_URL = "https://assets.breatheco.de/apis/fake/todos/user/maruchf";
 	const [tarea, guardarTarea] = useState("");
 	const [lista, guardarLista] = useState([]);
-	//crear una función para eliminar ítems del arreglo desde un botón
 	const eliminaItems = async (indexItem, listaActual) => {
-		//filtra la lista con todos los elementos menos el actual
 		const listFilter = listaActual.filter(
 			(todo, index) => index !== indexItem
 		);
-		//valida que no se está eliminando el último elemento de la lista y realiza
-		//una actualización de los elementos en la API con lo que está en listFilter
+
 		if (listFilter.length > 0) {
 			let response = await fetch(BASE_URL, {
 				method: "PUT",
-				body: JSON.stringify(listFilter), //Envía listFilter en .JSON
+				body: JSON.stringify(listFilter),
 				headers: {
 					"Content-Type": "application/json"
 				}
 			});
 			if (response.ok) {
-				guardarLista(listFilter); //cambia el estado si la respuesta de la API serv es ok, actualiza la lista
+				guardarLista(listFilter);
 			} else {
-				alert("hubo un problema"); //si la respuesta fue no ok, informa del problema en un alert
+				alert("hay un error");
 			}
 		} else {
-			//si es el último elemento de la lista entonces borra todo (duda: incluso el usuario?)
 			let response = await fetch(BASE_URL, {
 				method: "DELETE",
 				headers: {
@@ -35,9 +30,9 @@ function Home() {
 				}
 			});
 			if (response.ok) {
-				guardarLista([]); //si la respuesta anterior es ok, coloca la lista como un arreglo vacío.
+				guardarLista([]);
 			} else {
-				alert("Problema de nuevo"); //sino informa un problema
+				alert("Epa pana hay un error");
 			}
 		}
 	};
@@ -54,15 +49,11 @@ function Home() {
 		};
 		let url = `${BASE_URL}`;
 		obtenerTareas(url);
-		// fetch(url)
-		// 	.then(response => response.json())
-		// 	.then(APILista => guardarLista(APILista))
-		// 	.catch(error => console.log(`{error}`));
 	}, []);
 
 	return (
 		<div className="container-fluid col-8">
-			<h1 className="text-center">Lista de tareas</h1>
+			<h1 className="text-center">To do list</h1>
 			<div className="Group">
 				<input
 					value={tarea}
